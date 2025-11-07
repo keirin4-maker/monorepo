6724bb49d2597f42a239b4a44c7aef352bd94ab9
keirin4-maker <keirin4@gmail.com>
2025-11-07 09:56:49 -0700

    fix: correct relative import paths in api routes

--- apps/00-0_Resume_Forge/pages/api/create-checkout.ts
+++ apps/00-0_Resume_Forge/pages/api/create-checkout.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as LemonSqueezy from '@lemonsqueezy/lemonsqueezy.js'
-import { createSupabaseServerClient } from '../../../lib/supabase/server'
+import { createSupabaseServerClient } from '../../lib/supabase/server'
 
 // !!! REPLACE THIS with the Variant ID from your Lemon Squeezy product !!!
 const YOUR_VARIANT_ID = 12345; 
